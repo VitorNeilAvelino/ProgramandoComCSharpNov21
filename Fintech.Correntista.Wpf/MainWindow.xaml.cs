af56d7fff109ff0ed1ebd2eaad93799372d96752
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-11-16 21:39:28 -0300

    Aba clientes - inclusão de Cliente

--- Fintech.Correntista.Wpf/MainWindow.xaml.cs
+++ Fintech.Correntista.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fintech.Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,61 @@ namespace Fintech.Correntista.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Cliente> clientes = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            PopularControles();
+        }
+
+        private void PopularControles()
+        {
+            sexoComboBox.Items.Add(Sexo.Feminino);
+            sexoComboBox.Items.Add(Sexo.Masculino);
+            sexoComboBox.Items.Add(Sexo.Outros);
+
+            clienteDataGrid.ItemsSource = clientes;
         }
 
         private void incluirClienteButton_Click(object sender, RoutedEventArgs e)
         {
+            var cliente = new Cliente();
+            //Cliente cliente = new();
 
+            cliente.Cpf = cpfTextBox.Text;
+            cliente.DataNascimento = Convert.ToDateTime(dataNascimentoTextBox.Text);
+
+            Endereco endereco = new();
+            endereco.Cep = cepTextBox.Text;
+            endereco.Cidade = cidadeTextBox.Text;
+            endereco.Logradouro = logradouroTextBox.Text;
+            endereco.Numero = numeroLogradouroTextBox.Text;
+            
+            cliente.EnderecoResidencial = endereco;
+
+            cliente.Nome = nomeTextBox.Text;
+            cliente.Sexo = (Sexo)sexoComboBox.SelectedItem;
+
+            clientes.Add(cliente);
+
+            MessageBox.Show("Cliente cadastrado com sucesso!");
+            LimparControlesCliente();
+            pesquisaClienteTabItem.Focus();
+            clienteDataGrid.Items.Refresh();
+        }
+
+        private void LimparControlesCliente()
+        {
+            cpfTextBox.Clear();
+            nomeTextBox.Text = "";
+            dataNascimentoTextBox.Text = null;
+            sexoComboBox.SelectedIndex = -1;
+            
+            logradouroTextBox.Text = string.Empty;
+            numeroLogradouroTextBox.Clear();
+            cidadeTextBox.Clear();
+            cepTextBox.Clear();
         }
     }
 }
