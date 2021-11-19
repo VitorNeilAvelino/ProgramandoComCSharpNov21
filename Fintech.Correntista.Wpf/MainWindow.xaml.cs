629abb162cd40e17b388b83bb5620490638f95a0
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-11-18 22:34:47 -0300

    Finalização aba Contas

--- Fintech.Correntista.Wpf/MainWindow.xaml.cs
+++ Fintech.Correntista.Wpf/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Fintech.Correntista.Wpf
     public partial class MainWindow : Window
     {
         List<Cliente> clientes = new();
-
+        Cliente clienteSelecionado;
         public MainWindow()
         {
             InitializeComponent();
@@ -36,6 +36,17 @@ namespace Fintech.Correntista.Wpf
             sexoComboBox.Items.Add(Sexo.Outros);
 
             clienteDataGrid.ItemsSource = clientes;
+
+            tipoContaComboBox.Items.Add(TipoConta.ContaCorrente);
+            tipoContaComboBox.Items.Add(TipoConta.ContaEspecial);
+            tipoContaComboBox.Items.Add(TipoConta.Poupanca);
+
+            var banco1 = new Banco();
+            banco1.Nome = "Banco Um";
+            banco1.Numero = 938;
+
+            bancoComboBox.Items.Add(banco1);
+            bancoComboBox.Items.Add(new Banco { Nome = "Banco Dois", Numero = 942 });
         }
 
         private void incluirClienteButton_Click(object sender, RoutedEventArgs e)
@@ -51,7 +62,7 @@ namespace Fintech.Correntista.Wpf
             endereco.Cidade = cidadeTextBox.Text;
             endereco.Logradouro = logradouroTextBox.Text;
             endereco.Numero = numeroLogradouroTextBox.Text;
-            
+
             cliente.EnderecoResidencial = endereco;
 
             cliente.Nome = nomeTextBox.Text;
@@ -71,16 +82,88 @@ namespace Fintech.Correntista.Wpf
             nomeTextBox.Text = "";
             dataNascimentoTextBox.Text = null;
             sexoComboBox.SelectedIndex = -1;
-            
+
             logradouroTextBox.Text = string.Empty;
             numeroLogradouroTextBox.Clear();
             cidadeTextBox.Clear();
             cepTextBox.Clear();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SelecionarClienteButtonClick(object sender, RoutedEventArgs e)
         {
+            var botaoClicado = (Button)sender;
+            //var clienteSelecionado = botaoClicado.DataContext;
+            //this.clienteSelecionado = (Cliente)clienteSelecionado;
 
+            clienteSelecionado = (Cliente)botaoClicado.DataContext;
+
+            clienteTextBox.Text = $"{clienteSelecionado.Nome} - {clienteSelecionado.Cpf}";
+
+            contasTabItem.Focus();
+        }
+
+        private void tipoContaComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (tipoContaComboBox.SelectedItem == null) return;
+            
+            var tipoConta = (TipoConta)tipoContaComboBox.SelectedItem;
+
+            if (tipoConta == TipoConta.ContaEspecial)
+            {
+                limiteDockPanel.Visibility = Visibility.Visible;
+                limiteTextBox.Focus();
+            }
+            else
+            {
+                limiteDockPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void incluirContaButton_Click(object sender, RoutedEventArgs e)
+        {
+            Conta conta = null;
+
+            var agencia = new Agencia();
+            agencia.Banco = (Banco)bancoComboBox.SelectedItem;
+            agencia.Numero = Convert.ToInt32(numeroAgenciaTextBox.Text);
+            agencia.DigitoVerificador = Convert.ToInt32(dvAgenciaTextBox.Text);
+
+            var numero = Convert.ToInt32(numeroContaTextBox.Text);
+            var digitoVerificador = dvContaTextBox.Text;
+            var tipoConta = (TipoConta)tipoContaComboBox.SelectedItem;
+
+            switch (tipoConta)
+            {
+                case TipoConta.ContaCorrente:
+                    conta = new ContaCorrente(agencia, numero, digitoVerificador);
+                    break;
+                case TipoConta.ContaEspecial:
+                    var limite = Convert.ToDecimal(limiteTextBox.Text);
+                    conta = new ContaEspecial(agencia, numero, digitoVerificador, limite);
+                    break;
+                case TipoConta.Poupanca:
+                    conta = new Poupanca(agencia, numero, digitoVerificador);
+                    break;
+            }
+
+            clienteSelecionado.Contas.Add(conta);
+
+            MessageBox.Show("Conta adicionada com sucesso!");
+            LimparControlesConta();
+            clienteDataGrid.Items.Refresh();
+            clientesTabItem.Focus();
+        }
+
+        private void LimparControlesConta()
+        {
+            clienteTextBox.Clear();
+            bancoComboBox.SelectedIndex = -1;
+            numeroAgenciaTextBox.Clear();
+            dvAgenciaTextBox.Clear();
+            numeroContaTextBox.Clear();
+            dvContaTextBox.Clear();
+            tipoContaComboBox.SelectedIndex = -1;
+            limiteTextBox.Clear();
         }
     }
 }
---
+++ Fintech.Dominio/Entidades/TipoConta.cs
@@ -0,0 +1,10 @@
+﻿namespace Fintech.Dominio.Entidades
+{
+    public enum TipoConta
+    {
+        //[Description()]
+        ContaCorrente = 1,
+        ContaEspecial = 2,
+        Poupanca = 3
+    }
+}
