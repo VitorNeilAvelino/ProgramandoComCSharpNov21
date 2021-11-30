ced00ebb26d006de89d3c617a0bd47a729a3644b
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-11-29 23:08:37 -0300

    LINQ

---
+++ CSharp.Capitulo10.DelegatesLambda.Testes/DelegateTeste.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Capitulo10.DelegatesLambda.Testes
+{
+    [TestClass]
+    public class DelegateTeste
+    {
+        [TestMethod]
+        [DataRow(TipoOperacao.Soma, 1, 2, 3)]
+        [DataRow(TipoOperacao.Subtracao, 1, 2, -1)]
+        public void CalculadoraTeste(TipoOperacao tipoOperacao, int x, int y, int resultado)
+        {
+            var operacao = Calculadora.ObterOperacao(tipoOperacao);
+
+            Assert.IsTrue(operacao(x, y) == resultado);
+        }
+
+        [TestMethod]
+        public void MetodoAnonimoTeste()
+        {
+            EfetuarOperacao divisao = delegate (int x, int y)
+            {
+                return x / y;
+            };
+
+            Assert.IsTrue(divisao(6, 3) == 2);
+        }
+
+        [TestMethod]
+        public void ExpressaoLambdaTeste()
+        {
+            EfetuarOperacao multiplicacao = (x, y) =>
+            {
+                return x * y;
+            };
+
+            EfetuarOperacao multiplicacao2 = (x, y) => x * y;
+
+            Assert.IsTrue(multiplicacao2(6, 3) == 18);
+        }
+    }
+}
--- Fintech.Repositorios.SistemaArquivosTests/MovimentoRepositorioTests.cs
+++ Fintech.Repositorios.SistemaArquivosTests/MovimentoRepositorioTests.cs
@@ -22,7 +22,7 @@ namespace Fintech.Repositorios.SistemaArquivos.Tests
 
             var conta = new ContaCorrente(agencia, 1, "1");
 
-            var movimento = new Movimento(Operacao.Deposito, 10);
+            var movimento = new Movimento(Operacao.Saque, 20);
             movimento.Conta = conta;
 
             repositorio.Inserir(movimento);
@@ -36,6 +36,81 @@ namespace Fintech.Repositorios.SistemaArquivos.Tests
             foreach (var movimento in movimentos)
             {
                 Console.WriteLine($"{movimento.Data}: {movimento.Operacao} {movimento.Valor:c}");
+            }
+        }
+
+        [TestMethod]
+        public void OrderByTeste()
+        {
+            var movimentos = repositorio.Selecionar(2, 1)
+                .OrderBy(m => m.Valor)
+                .ThenBy(m => m.Operacao)
+                .ThenByDescending(m => m.Data);
+
+            foreach (var movimento in movimentos)
+            {
+                Console.WriteLine($"{movimento.Data}: {movimento.Operacao} {movimento.Valor:c}");
+            }
+        }
+
+        [TestMethod]
+        public void CountTeste()
+        {
+            var qtdDepositos = repositorio.Selecionar(2, 1)
+                .Count(m => m.Operacao == Operacao.Deposito);
+
+            Assert.AreEqual(qtdDepositos, 2);
+        }
+
+        [TestMethod]
+        public void LikeTeste()
+        {
+            var movimentos = repositorio.Selecionar(2, 1)
+                .Where(m => m.Data.ToString().Contains("29/11/2021"));
+
+            //Assert.IsTrue(movimentos.Count() == 0);
+
+            foreach (var movimento in movimentos)
+            {
+                Console.WriteLine($"{movimento.Data}: {movimento.Operacao} {movimento.Valor:c}");
+            }
+        }
+
+        [TestMethod]
+        public void MinTeste()
+        {
+            var menorDeposito = repositorio.Selecionar(2, 1)
+                .Where(m => m.Operacao == Operacao.Deposito)
+                .Min(m => m.Valor);
+
+            Assert.IsTrue(menorDeposito == 10);
+        }
+
+        [TestMethod]
+        public void SkipTakeTeste()
+        {
+            var movimentos = repositorio.Selecionar(2, 1)
+                .Skip(1)
+                .Take(5)
+                .ToList();
+
+            foreach (var movimento in movimentos)
+            {
+                Console.WriteLine($"{movimento.Data}: {movimento.Operacao} {movimento.Valor:c}");
+            }
+        }
+
+        [TestMethod]
+        public void GroupByTeste()
+        {
+            var agrupamento = repositorio.Selecionar(2, 1)
+                //.GroupBy(m => m.Operacao)
+                .GroupBy(m => new { m.Operacao, m.Data })
+                .Select(g => new { Operacao = g.Key.Operacao, Data = g.Key.Data.ToShortDateString(), Total = g.Sum(m => m.Valor) });
+
+            foreach (var item in agrupamento)
+            {
+                Console.WriteLine($"{item.Operacao} - {item.Data}: {item.Total:c}");
             }
         }
     }
